a470216ec928b0d56fc096aa3bd965f0205dbbf4
Buğra Coşkun <45716038+bugracoskun@users.noreply.github.com>
2024-01-16 17:57:43 +0300

    Login card eklendi.

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,4 +1,7 @@
-import { Button, Form } from 'react-bootstrap';
+'use client'
+import { Button, Form, Card } from 'react-bootstrap';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { PersonFill, LockFill } from 'react-bootstrap-icons';
 import styles from './login.css';
 
 
@@ -15,19 +18,33 @@ export default function loginPage() {
         </div>
 
 
-        <div cclassName="box2-2">
-          <br />
-          <div className='form'>
-            <h3>Hoşgeldiniz</h3>
-            <br />
-            <Form.Control type="email" placeholder="E-mail" />
-            <br />
-            <Form.Control type="password" placeholder="Şifre" />
-            <br />
-            <Button className='btn' variant="primary" type="submit">Giriş Yap</Button>
-            <p>Hesabınız yok mu? Hemen <a href="URL">kayıt ol</a></p>
-          </div>
-        </div>
+        <Card className="shadow">
+          <Card.Body>
+            <div cclassName="box2-2">
+              <br />
+              <div className='form'>
+                <h3>Hoşgeldiniz</h3>
+                <br />
+                <InputGroup className="mb-3">
+                  <InputGroup.Text><PersonFill size={17} /></InputGroup.Text>
+                  <Form.Control
+                    placeholder="Username"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                  />
+                </InputGroup>
+                <br />
+                <InputGroup className="mb-3">
+                  <InputGroup.Text><LockFill size={17} /></InputGroup.Text>
+                  <Form.Control type="password" placeholder="Şifre" />
+                </InputGroup>
+                <br />
+                <Button className='btn' variant="primary" type="submit">Giriş Yap</Button>
+                <p>Hesabınız yok mu? Hemen <a href="URL">kayıt ol</a></p>
+              </div>
+            </div>
+          </Card.Body>
+        </Card>
 
       </div>
 
